63cb3dd6f49bffc7deb99e930a027bfcc7750ebc
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2016-07-03 15:21:19 +0300

    Add simple main loop.

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import halosim.crystals
 import halosim.plotting
 import numpy as np
 
+NUM_RAYS = 3
+PLOT_EVERY_CRYSTAL = False
+
 
 def main():
-    vertices, triangles = halosim.crystals.generate_hexagonal_crystal()
+    for ray_idx in range(NUM_RAYS):
+        vertices, triangles = halosim.crystals.generate_hexagonal_crystal()
 
-    rotate_a = 0.01 * 2.0 * np.pi * np.random.randn()
-    rotate_b = 0.01 * 2.0 * np.pi * np.random.randn()
-    rotate_c = 2.0 * np.pi * np.random.rand()
-    vertices = halosim.crystals.rotate(vertices, rotate_a, rotate_b, rotate_c)
+        rotate_a = 0.01 * 2.0 * np.pi * np.random.randn()
+        rotate_b = 0.01 * 2.0 * np.pi * np.random.randn()
+        rotate_c = 2.0 * np.pi * np.random.rand()
+        vertices = halosim.crystals.rotate(vertices, rotate_a, rotate_b, rotate_c)
 
-    normals = halosim.crystals.get_normals(vertices, triangles)
+        normals = halosim.crystals.get_normals(vertices, triangles)
 
-    halosim.plotting.plot_crystal(vertices, triangles)
+        if PLOT_EVERY_CRYSTAL:
+            halosim.plotting.plot_crystal(vertices, triangles)
 
 if __name__ == '__main__':
     main()
