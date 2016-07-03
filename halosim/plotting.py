1e54a9be381ca0979914c6b300e1411aaed317fd
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2016-07-03 14:40:06 +0300

    Change crystal color and transparency.

--- halosim/plotting.py
+++ halosim/plotting.py
@@ -7,7 +7,7 @@ def plot_crystal(vertices, triangles):
     ax = fig.add_subplot(111, projection='3d')
     for triangle in triangles:
         vertices_in_triangle = [vertices[:, triangle].T]
-        ax.add_collection3d(Poly3DCollection(vertices_in_triangle))
+        ax.add_collection3d(Poly3DCollection(vertices_in_triangle, facecolors='w', alpha=0.5))
 
     ax.set_xlim(-1.0, 1.0)
     ax.set_ylim(-1.0, 1.0)
@@ -16,5 +16,6 @@ def plot_crystal(vertices, triangles):
     ax.set_ylabel('y')
     ax.set_zlabel('z')
     ax.set_aspect('equal')
+    fig.tight_layout()
 
     plt.show()
