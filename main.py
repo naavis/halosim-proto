32c25ec1d861f78fbbb4c36444a726283fdf70ac
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2016-07-03 17:30:55 +0300

    Check which triangles are visible to ray.

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import numpy as np
 NUM_RAYS = 3
 PLOT_EVERY_CRYSTAL = False
 
+# Alt/az in degrees
+SUN_AZIMUTH = 180.0
+SUN_ALTITUDE = 30.0
+
 
 def main():
     for ray_idx in range(NUM_RAYS):
@@ -16,6 +20,27 @@ def main():
         vertices = halosim.crystals.rotate(vertices, rotate_a, rotate_b, rotate_c)
 
         normals = halosim.crystals.get_normals(vertices, triangles)
+
+        # Generate ray
+        # Currently the ray is always the same; later on direction can be randomized on sun's disk.
+        ray = np.array([0.0, 0.0, 1.0])
+        ray = np.array([
+            ray[0],
+            np.cos(SUN_ALTITUDE) - np.sin(SUN_ALTITUDE),
+            np.sin(SUN_ALTITUDE) + np.cos(SUN_ALTITUDE)
+        ])
+        ray = np.array([
+            np.cos(SUN_AZIMUTH) - np.sin(SUN_AZIMUTH),
+            ray[1],
+            np.sin(SUN_AZIMUTH) + np.cos(SUN_AZIMUTH)
+        ])
+
+        visible_triangles = []
+        for triangle_idx, triangle in enumerate(triangles):
+            normal = normals[:, triangle_idx]
+            dot_product = np.dot(normal, ray)
+            if dot_product > 0.0:
+                visible_triangles.append(triangle_idx)
 
         if PLOT_EVERY_CRYSTAL:
             halosim.plotting.plot_crystal(vertices, triangles)
