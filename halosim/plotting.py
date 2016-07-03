90cf7d72890aedb911ed99e35425053a51858cef
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2016-07-03 14:47:49 +0300

    Change axis order when plotting.

--- halosim/plotting.py
+++ halosim/plotting.py
@@ -6,15 +6,16 @@ def plot_crystal(vertices, triangles):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     for triangle in triangles:
-        vertices_in_triangle = [vertices[:, triangle].T]
+        vertices_in_triangle = vertices[:, triangle].T
+        vertices_in_triangle = [vertices_in_triangle[:, [2, 0, 1]]]
         ax.add_collection3d(Poly3DCollection(vertices_in_triangle, facecolors='w', alpha=0.5))
 
     ax.set_xlim(-1.0, 1.0)
     ax.set_ylim(-1.0, 1.0)
     ax.set_zlim(-1.0, 1.0)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    ax.set_xlabel('z')
+    ax.set_ylabel('x')
+    ax.set_zlabel('y')
     ax.set_aspect('equal')
     fig.tight_layout()
 
