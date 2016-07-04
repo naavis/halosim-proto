dbb74ab5d37950949fea63c4c67d5a5a8b170d3c
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2016-07-04 15:40:24 +0300

    Calculate reflectivity.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from halosim.crystals import generate_hexagonal_crystal
 from halosim.plotting import plot_crystal
 from halosim.raytracing import generate_primary_ray, get_primary_intersection
 
-NUM_RAYS = 5
+NUM_RAYS = 10
 PLOT_EVERY_CRYSTAL = True
 
 # Alt/az
@@ -20,13 +20,26 @@ def main():
         vertices, triangles, normals, areas = generate_hexagonal_crystal(rotation_std, rotation_std, c_a_ratio)
 
         # Generate ray
-        ray = generate_primary_ray(SUN_AZIMUTH, SUN_ALTITUDE)
+        sun_direction = generate_primary_ray(SUN_AZIMUTH, SUN_ALTITUDE)
 
         # Find primary ray intersection point
-        hit_triangle_index, intersection = get_primary_intersection(vertices, triangles, normals, areas, ray)
+        hit_triangle_index, intersection = get_primary_intersection(vertices, triangles, normals, areas, sun_direction)
+        hit_triangle_normal = normals[:, hit_triangle_index]
+
+        incident_angle = np.arccos(np.dot(sun_direction, hit_triangle_normal))
+        transmitted_angle = np.arcsin(np.sin(incident_angle) / 1.31)
+        reflectivity_parallel = np.square(np.cos(incident_angle) - 1.31 * np.cos(transmitted_angle) / (
+            np.cos(incident_angle) + np.cos(transmitted_angle)))
+        reflectivity_perpendicular = np.square(np.cos(transmitted_angle) - 1.31 * np.cos(incident_angle) / (
+            np.cos(transmitted_angle) + np.cos(incident_angle)))
+        reflectivity = 0.5 * (reflectivity_parallel + reflectivity_perpendicular)
+        if np.random.rand() < reflectivity:
+            pass  # Reflect
+        else:
+            pass  # Refract
 
         if PLOT_EVERY_CRYSTAL:
-            plot_crystal(vertices, triangles, ray, highlight_triangle=hit_triangle_index, point=intersection)
+            plot_crystal(vertices, triangles, sun_direction, highlight_triangle=hit_triangle_index, point=intersection)
 
 
 if __name__ == '__main__':
